38b27ebad2ed9e1ae3e03603f43d38a2e5128fe1
Greg McGhee <gmcghee64@verizon.net>
2018-03-18 15:44:53 -0400

    Initial Commit

---
+++ TaskManager/Main.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MyMainMenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.MyMainStatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CloseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MyMainMenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MyMainMenuStrip
+        '
+        Me.MyMainMenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MyMainMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MyMainMenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MyMainMenuStrip.Name = "MyMainMenuStrip"
+        Me.MyMainMenuStrip.Size = New System.Drawing.Size(800, 28)
+        Me.MyMainMenuStrip.TabIndex = 0
+        Me.MyMainMenuStrip.Text = "MenuStrip1"
+        '
+        'MyMainStatusStrip
+        '
+        Me.MyMainStatusStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MyMainStatusStrip.Location = New System.Drawing.Point(0, 428)
+        Me.MyMainStatusStrip.Name = "MyMainStatusStrip"
+        Me.MyMainStatusStrip.Size = New System.Drawing.Size(800, 22)
+        Me.MyMainStatusStrip.TabIndex = 1
+        Me.MyMainStatusStrip.Text = "StatusStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.OpenToolStripMenuItem, Me.CloseToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.NewToolStripMenuItem.Text = "New"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'CloseToolStripMenuItem
+        '
+        Me.CloseToolStripMenuItem.Name = "CloseToolStripMenuItem"
+        Me.CloseToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.CloseToolStripMenuItem.Text = "Close"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MyMainStatusStrip)
+        Me.Controls.Add(Me.MyMainMenuStrip)
+        Me.MainMenuStrip = Me.MyMainMenuStrip
+        Me.Name = "Main"
+        Me.Text = "Task Manager"
+        Me.MyMainMenuStrip.ResumeLayout(False)
+        Me.MyMainMenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MyMainMenuStrip As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CloseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MyMainStatusStrip As StatusStrip
+End Class
